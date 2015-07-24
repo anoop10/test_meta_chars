6f30c24b410ab72ec93e14cedc9bb30956ff791c
Anoop Singh <sanoop@zeomega.com>
2015-07-24 16:43:23 +0530

    Commit 1 with Meta characters

---
+++ test_meta_char.js
@@ -0,0 +1,45 @@
+/* *******************************************************************************^M
+#ZeOmega LLC license^M
+#Copyright (c) 2003-2008 ZeOmega LLC^M
+#http://www.zeomega.com^M
+#All rights reserved.^M
+#ZeOmega software [both binary and source (if released)] (hereafter, Software)^M
+#is intellectual property owned by ZeOmega LLC is copyright of ZeOmega LLC^M
+#in all countries in the world, and ownership remains with ZeOmega LLC.^M
+#^M
+#The Software is protected by the copyright laws of the United States and^M
+#international copyright treaties. Licensee is not allowed to distribute^M                                                                                                       
+#the binary and source code (if released) to third parties. Licensee is not^M
+#allowed to reverse engineer, disassemble or decompile code, or make any^M
+#modifications of the binary or source code, remove or alter any trademark,^M
+#logo, copyright or other proprietary notices, legends, symbols, or labels in^M
+#the Software.^M
+#^M
+#Licensee is not allowed to sub-license the Software or any derivative work^M
+#based on or derived from the Software. Neither the names of ZeOmega LLC ,^M
+#nor the names of its contributors may be used to endorse or promote products^M
+#derived from this Software without specific prior written permission.^M
+#^M
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR^M
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,^M
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL^M
+#THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR^M
+#OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,^M
+#ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER^M
+#DEALINGS WITH THE SOFTWARE.^M
+******************************************************************************** */^M
+
+
+^M
+YAHOO.namespace('Ze.SCRIPT.ACE');^M
+YAHOO.Ze.SCRIPT.ACE = new function ()^M
+{^M
+        this.logName="ZeACE";^M
+            ^M
+            /**^M
+             * @description : This function is used in the search titles/screens page,^M
+             * it is used to get the record set and display in the search result page.^M
+             * @param {String} sFrmName : Form name from where this function is getting called^M
+             * @param {Integer} iCurPage : Paginations current page.^M
+             **/
+}
